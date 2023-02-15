61005b57ae0635b23300876ffb895cca0a6887ba
Yongdun Yang <yongdun.yang@kuku.tech>
2023-02-16 08:08:11 +1300

    tweak comment shortcut wiht leader key

--- .config/nvim/after/plugin/comment.rc.lua
+++ .config/nvim/after/plugin/comment.rc.lua
@@ -1,4 +1,30 @@
 local status, com = pcall(require, "Comment")
 if (not status) then return end
 
-com.setup()
+com.setup(
+    {
+        ---LHS of toggle mappings in NORMAL mode
+        toggler = {
+            ---Line-comment toggle keymap
+            line = '<leader>cc',
+            ---Block-comment toggle keymap
+            block = '<leader>bc',
+        },
+        ---LHS of operator-pending mappings in NORMAL and VISUAL mode
+        opleader = {
+            ---Line-comment keymap
+            line = '<leader>c',
+            ---Block-comment keymap
+            block = '<leader>b',
+        },
+        ---LHS of extra mappings
+        extra = {
+            ---Add comment on the line above
+            above = '<leader>cO',
+            ---Add comment on the line below
+            below = '<leader>co',
+            ---Add comment at the end of line
+            eol = '<leader>cA',
+        },
+    }
+)
